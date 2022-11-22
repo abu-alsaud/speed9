97720c75712e3d8f95e97f37924be84fd637794f
Mohamed Abu Al-Saud <118353596+abu-alsaud@users.noreply.github.com>
2022-11-22 18:51:14 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,126 @@
+let A1 = document.getElementById('A1');
+let A2 = document.getElementById('A2');
+let A3 = document.getElementById('A3');
+let A4 = document.getElementById('A4');
+let A5 = document.getElementById('A5');
+let A6 = document.getElementById('A6');
+let A7 = document.getElementById('A7');
+let A8 = document.getElementById('A8');
+let A9 = document.getElementById('A9');
+let A = document.getElementById('A');
+let B = document.getElementById('B');
+let C = document.getElementById('C');
+let D = document.getElementById('D');
+let E = document.getElementById('E');
+let F = document.getElementById('F');
+let G = document.getElementById('G');
+let H = document.getElementById('H');
+let I = document.getElementById('I');
+
+A.onclick = function(){
+    A1.classList.remove('hidng')
+    A2.classList.add('hidng')
+    A3.classList.add('hidng')
+    A4.classList.add('hidng')
+    A5.classList.add('hidng')
+    A6.classList.add('hidng')
+    A7.classList.add('hidng')
+    A8.classList.add('hidng')
+    A9.classList.add('hidng')
+}
+
+B.onclick = function(){
+    A2.classList.remove('hidng')
+    A1.classList.add('hidng')
+    A3.classList.add('hidng')
+    A4.classList.add('hidng')
+    A5.classList.add('hidng')
+    A6.classList.add('hidng')
+    A7.classList.add('hidng')
+    A8.classList.add('hidng')
+    A9.classList.add('hidng')
+}
+
+C.onclick = function(){
+    A3.classList.remove('hidng')
+    A1.classList.add('hidng')
+    A2.classList.add('hidng')
+    A4.classList.add('hidng')
+    A5.classList.add('hidng')
+    A6.classList.add('hidng')
+    A7.classList.add('hidng')
+    A8.classList.add('hidng')
+    A9.classList.add('hidng')
+}
+
+D.onclick = function(){
+    A4.classList.remove('hidng')
+    A1.classList.add('hidng')
+    A2.classList.add('hidng')
+    A3.classList.add('hidng')
+    A5.classList.add('hidng')
+    A6.classList.add('hidng')
+    A7.classList.add('hidng')
+    A8.classList.add('hidng')
+    A9.classList.add('hidng')
+}
+
+E.onclick = function(){
+    A5.classList.remove('hidng')
+    A1.classList.add('hidng')
+    A2.classList.add('hidng')
+    A3.classList.add('hidng')
+    A4.classList.add('hidng')
+    A6.classList.add('hidng')
+    A7.classList.add('hidng')
+    A8.classList.add('hidng')
+    A9.classList.add('hidng')
+}
+
+F.onclick = function(){
+    A6.classList.remove('hidng')
+    A1.classList.add('hidng')
+    A2.classList.add('hidng')
+    A3.classList.add('hidng')
+    A4.classList.add('hidng')
+    A5.classList.add('hidng')
+    A7.classList.add('hidng')
+    A8.classList.add('hidng')
+    A9.classList.add('hidng')
+}
+
+G.onclick = function(){
+    A7.classList.remove('hidng')
+    A1.classList.add('hidng')
+    A2.classList.add('hidng')
+    A3.classList.add('hidng')
+    A4.classList.add('hidng')
+    A5.classList.add('hidng')
+    A6.classList.add('hidng')
+    A8.classList.add('hidng')
+    A9.classList.add('hidng')
+}
+
+H.onclick = function(){
+    A8.classList.remove('hidng')
+    A1.classList.add('hidng')
+    A2.classList.add('hidng')
+    A3.classList.add('hidng')
+    A4.classList.add('hidng')
+    A5.classList.add('hidng')
+    A6.classList.add('hidng')
+    A7.classList.add('hidng')
+    A9.classList.add('hidng')
+}
+
+I.onclick = function(){
+    A9.classList.remove('hidng')
+    A1.classList.add('hidng')
+    A2.classList.add('hidng')
+    A3.classList.add('hidng')
+    A4.classList.add('hidng')
+    A5.classList.add('hidng')
+    A6.classList.add('hidng')
+    A7.classList.add('hidng')
+    A8.classList.add('hidng')
+}
